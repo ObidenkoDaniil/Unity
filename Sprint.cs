b59f48373d09afa3fd08fe2489fe96f2e95990b4
ObidenkoDaniil <151772296+ObidenkoDaniil@users.noreply.github.com>
2023-11-29 20:14:25 +0300

    Add files via upload

---
+++ Sprint.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sprint : MonoBehaviour
+{
+    public int health = 10;
+    public int lvl = 1;
+    public float speed = 1.2f;
+    
+
+    void Start()
+    {
+        health += lvl;
+        print("Çהמנמגüו " + health);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = transform.position;
+        newPosition.z += speed * Time.deltaTime;
+        transform.position = newPosition;
+    }
+}
